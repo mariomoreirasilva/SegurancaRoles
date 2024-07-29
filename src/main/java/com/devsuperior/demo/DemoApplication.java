f78a37d811824c80a73a5c7ab0f819c3455094e7
mariomoreirasilva <mario@desban.org.br>
2024-07-29 10:35:03 -0300

    Spring Security implementado e não testado

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -9,24 +9,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import ch.qos.logback.core.net.SyslogOutputStream;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
+public class DemoApplication  {
 	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Codificando 123456 = " + passwordEncoder.encode("123456"));
-		
-		//testando a encriptação
-		boolean result = passwordEncoder.matches("123456", "$2a$10$XTE7jFPZXgoGDGAqgshULuBXtIUbmRAVBUU62VFqbj4QBrYqYnuQq");
-		System.out.println("Teste do encoder = " + result);
-				
-		
-	}
+	
 
 }
